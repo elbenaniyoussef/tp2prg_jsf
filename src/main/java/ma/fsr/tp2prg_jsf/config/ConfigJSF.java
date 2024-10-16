551449877860325762a39c96efd1ab639babe2ca
elbenani <elbenani@gmail.com>
2024-10-16 10:58:23 +0100

    config TP2

---
+++ src/main/java/ma/fsr/tp2prg_jsf/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.fsr.tp2prg_jsf.config; // A MODIFIER suivant le paquetage de base...
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ *
+ * @author xxxx
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
